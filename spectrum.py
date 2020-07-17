9e76f147662c8c10518dc73859eb356681d37463
Vladislav Radchenko <56233697+VladicNaAmure@users.noreply.github.com>
2020-07-17 14:09:42 +0300

    Update spectrum.py
    Corrected the outfile method to save tsv

--- spectrum.py
+++ spectrum.py
@@ -71,8 +71,12 @@ def result(A,n,size):
 
 def outfile(path):
     with open("file.txt", "w") as output:
-        for i in range(len(path[0])):
-            output.write(str(i)+'    '+str(path[0][i])+'\n')    # Приводим в нужный формат. TSV без заголовка: 2 столбца (вес, количество), разделитель полей - HT, разделитель записей - LF.
+        if len(path) != 1:
+            for i in range(len(path)):
+                output.write(str(i)+'    '+str(path[i])+'\n')    # Приводим в нужный формат. TSV без заголовка: 2 столбца (вес, количество), разделитель полей - HT, разделитель записей - LF.
+        else:
+            for i in range(len(path[0])):
+                output.write(str(i)+'    '+str(path[0][i])+'\n') 
 
 def main(path):
     A,n,size = read(path)
